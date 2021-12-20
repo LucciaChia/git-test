6b528b12129dc4b980c06aad7d5abd147d647e9d
LucciaChia <krausovalucia23@gmail.com>
2021-12-20 10:43:25 +0100

    StringBuilder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello There!");
+
+        String [] words = {"hello", "world"};
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<ul>\n");
+        for (String word : words) {
+            sb.append(String.format("  <li>%s</li>\n", word));
+        }
+        sb.append("</ul>");
+        System.out.println(sb);
     }
 }
